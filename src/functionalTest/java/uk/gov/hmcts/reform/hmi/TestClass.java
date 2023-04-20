bc0fa7bc81ecb9ebdc7060e1e0fd0e99762acfe8
Josh Blackmoor <joshblackmoor01@icloud.com>
2023-04-20 09:43:16 +0100

    Added temp tests

---
+++ src/functionalTest/java/uk/gov/hmcts/reform/hmi/TestClass.java
@@ -0,0 +1,18 @@
+package uk.gov.hmcts.reform.hmi;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TestClass {
+
+    /**
+     * Placeholder for Functional Tests.
+     */
+    @Test
+    void placeholderTest() {
+        assertEquals("Placeholder Test", "Placeholder Test",
+                     "Placeholder test for functional tests");
+    }
+
+}
